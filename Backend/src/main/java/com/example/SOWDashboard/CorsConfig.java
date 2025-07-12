0b0e525c5938aa0437648734540df773e822708a
Abisek17 <srabisek@gmail.com>
2025-07-12 17:09:32 +0530

    Recent Commit

--- Backend/src/main/java/com/example/SOWDashboard/CorsConfig.java
+++ Backend/src/main/java/com/example/SOWDashboard/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**")
-                        .allowedOrigins("https://sow-dash-board-82g8errcy-abiseks-projects-56e72be1.vercel.app")
+                        .allowedOrigins("https://sow-dash-board-300ztjxhm-abiseks-projects-56e72be1.vercel.app/")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*");
             }
